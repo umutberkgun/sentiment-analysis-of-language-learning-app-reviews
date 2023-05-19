010a83e6ce0a1bb03432fc64b0393521d60a30ab
umutberkgun <103524277+umutberkgun@users.noreply.github.com>
2023-05-19 11:25:13 +0200

    Update preprocessing_and_sentiment_analysis.py

--- preprocessing_and_sentiment_analysis.py
+++ preprocessing_and_sentiment_analysis.py
@@ -8,7 +8,7 @@ import re
 import pandas as pd
 sia = SentimentIntensityAnalyzer()
 
-def process(text):
+def process(text): #removing stopwords,punctuations, numbers etc.
     text = text.lower()
     text = text.translate(str.maketrans("","",string.punctuation))
     text = re.sub(r'\d+', '', text)
@@ -22,12 +22,12 @@ def process(text):
 duolingo_review_texts = open("duolingoreviews.txt","r").read()
 processed_duolingo_text = process(duolingo_review_texts)
 
-dictionary_of_duolingo_tokens = {}
+dictionary_of_duolingo_tokens = {} #used dictionary to store both the token and the sentiment
 
 for i in processed_duolingo_text:
     dictionary_of_duolingo_tokens[str(i)] = sia.polarity_scores(i)
 
-conn_duo = sqlite3.connect("duolingo_tokens.db")
+conn_duo = sqlite3.connect("duolingo_tokens.db") #utilized sqlite3 to store the data
 cursor = conn_duo.cursor()
 
 cursor.execute("CREATE TABLE IF NOT EXISTS duolingo_review_tokens (token TEXT,neu_score REAL,neg_score REAL,pos_score REAL,compound_score REAL)")
@@ -35,7 +35,7 @@ conn_duo.commit()
 
 
 for value,key in dictionary_of_duolingo_tokens.items():
-    if isinstance(value,int):
+    if isinstance(value,int): #I don't need numbers
         continue
     else:
         sql_statement = f"INSERT INTO duolingo_review_tokens (token,neu_score,neg_score,pos_score,compound_score) VALUES('{value}',{dictionary_of_duolingo_tokens[value]['neu']},{dictionary_of_duolingo_tokens[value]['neg']},{dictionary_of_duolingo_tokens[value]['pos']},{dictionary_of_duolingo_tokens[value]['compound']})"
@@ -43,7 +43,7 @@ for value,key in dictionary_of_duolingo_tokens.items():
         conn_duo.commit()
 
 df_duolingo = pd.read_sql_query("SELECT * FROM duolingo_review_tokens ",conn_duo)
-print(df_duolingo)
+print(df_duolingo)#to check if everything works
 
 
 rosetta_stone_review_text = open("rosettastonereviews.txt","r").read()
